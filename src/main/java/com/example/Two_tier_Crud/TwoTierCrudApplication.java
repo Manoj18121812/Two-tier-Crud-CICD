31b20663ae5a4402cf6233d5e22be8cc3d7267e3
Manoj Gavhane <131341403+Manoj18121812@users.noreply.github.com>
2026-02-11 19:42:29 +0530

    two tier crud application with cicd

---
+++ src/main/java/com/example/Two_tier_Crud/TwoTierCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.Two_tier_Crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoTierCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwoTierCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Two_tier_Crud/TwoTierCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Two_tier_Crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwoTierCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
